fb696bf502b4c3e384037543dbe64c41d9382e53
Евгений Примаков <conf@confusion.su>
2020-05-29 10:50:36 +0300

    Добавил .dat файлы с скрипт сборки проекта

--- vivado_project.tcl
+++ vivado_project.tcl
@@ -296,6 +296,9 @@ set files [list \
  [file normalize "${origin_dir}/submodules/riscv/riscv_prefetch_buffer.sv"] \
  [file normalize "${origin_dir}/submodules/riscv/riscv_register_file.sv"] \
  [file normalize "${origin_dir}/rtl/xilinx_spram.v"] \
+ [file normalize "${origin_dir}/sw/test_sw_emb_text.dat"] \
+ [file normalize "${origin_dir}/sw/test_sw_emb_data.dat"] \
+ [file normalize "${origin_dir}/sw/boot_code.dat"] \
 ]
 
 # [file normalize "${origin_dir}/submodules/riscv/riscv_tracer.sv"] \
@@ -1067,6 +1070,25 @@ set file "$origin_dir/submodules/riscv/riscv_register_file.sv"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
+
+
+
+set file "$origin_dir/sw/test_sw_emb_text.dat"
+set file [file normalize $file]
+set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
+set_property -name "file_type" -value "Data Files" -objects $file_obj
+
+set file "$origin_dir/sw/test_sw_emb_data.dat"
+set file [file normalize $file]
+set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
+set_property -name "file_type" -value "Data Files" -objects $file_obj
+
+set file "$origin_dir/sw/boot_code.dat"
+set file [file normalize $file]
+set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
+set_property -name "file_type" -value "Data Files" -objects $file_obj
+
+
 
 
 #set file "$origin_dir/rtl/xilinx_spram.v"
